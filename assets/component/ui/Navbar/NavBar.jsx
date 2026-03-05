d6b3aece25a09ab796b6cc0c2bdfa0420f7e523b
ThomasDlv0 <thomas.delville000@icloud.com>
2026-03-05 13:32:05 +0100

    rôles/session presque fini a voir avec l'équipe

--- assets/component/ui/Navbar/NavBar.jsx
+++ assets/component/ui/Navbar/NavBar.jsx
@@ -149,7 +149,7 @@ const NavBar = ({user}) => {
                                                    className="block w-full text-left px-3 py-2 rounded-lg hover:bg-slate-100 text-slate-700">
                                                     Recherche maps
                                                 </a>
-                                                <a href="/maps"
+                                                <a href="/adminClient"
                                                    className="block w-full text-left px-3 py-2 rounded-lg hover:bg-slate-100 text-slate-700">
                                                     Dashborad Entreprise
                                                 </a>
